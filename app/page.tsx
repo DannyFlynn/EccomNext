531f284bfa099bad80a3eaed15af6968ab0a88bf
Danny Flynn <flynny386@gmail.com>
2024-11-27 10:41:51 +0000

    first page and navigation component tidy up and adjusting to fit screen sizes

--- app/page.tsx
+++ app/page.tsx
@@ -52,19 +52,19 @@ export default function Home() {
   // };
 
   return (
-    <div className="w-full lg:h-screen lg:overflow-hidden overflow-x-hidden relative">
+    <div className="w-full lg:h-screen lg:overflow-hidden overflow-x-hidden relative text-white text-shadow">
       <div className="w-full h-full flex flex-col items-center">
         <NavigationContainer />
         <div className="w-full h-[85%] flex flex-col">
           <div className="w-full  h-[10%] p-3 flex justify-center lg:justify-end">
-            <div className="w-4/5 sm:w-2/4 lg:w-1/5 relative">
-              <FaSearch className="absolute right-1 top-2" size={22} />
-              <Input type="search" placeholder="Search..." />
+            <div className="w-4/5 sm:w-2/4 lg:w-2/6 2xl:w-1/5 relative my-6 text-black">
+              <FaSearch className="absolute left-1 top-2 text-black" size={22} />
+              <Input type="search" placeholder="Search..." className="pl-10 py-3 border-2 border-indigo-200 focus:border-indigo-500" />
             </div>
           </div>
-          <div className="w-full text-center h-[10%] p-3">
+          {/* <div className="w-full text-center h-[10%] p-3 text-black">
             <h3 className="text-3xl">Choose a category</h3>
-          </div>
+          </div> */}
           <div className="w-full p-3 flex flex-col sm:flex-row justify-evenly items-center lg:h-[60%]">
             {items.map((item, index) => (
               <Card
@@ -89,11 +89,11 @@ export default function Home() {
             ))}
           </div>
         </div>
-        <footer className="w-full h-20  block lg:hidden border-t bg-indigo-200 text-white">
+        <footer className="w-full h-20  block lg:hidden border-t bg-indigo-200 ">
           I am mob FOOT
         </footer>
       </div>
-      <footer className="w-full h-[15%] absolute bottom-0  hidden lg:block border-t bg-indigo-200 text-white">
+      <footer className="w-full h-[10%] absolute bottom-0  hidden lg:block border-t bg-indigo-200 ">
         I am web FOOT
       </footer>
     </div>
--- components/ui/Navigation.tsx
+++ components/ui/Navigation.tsx
@@ -14,21 +14,24 @@ const Navigation = ({ menuOpen, toggleMenu }: NavigationProps) => {
   return (
     <nav className="w-full h-20 bg-indigo-200 lg:h-[15%] flex border-b relative">
       <div className="hidden lg:block w-1/4  p-3">
-        <div className="hidden lg:w-2/3 lg:flex justify-around">
-          <Link href="/">Woman</Link>
-          <Link href="/home">Men</Link>
-          <Link href="/home">Kids</Link>
+        <div className="hidden lg:w-2/3 lg:flex justify-around text-2xl">
+          <Link href="/" >Woman</Link>
+          <Link href="/home" className="ml-3">Men</Link>
+          <Link href="/home" className="ml-3">Kids</Link>
         </div>
       </div>
-      <div className="w-2/4  p-3 lg:text-center text-white">
-        <h1 className={`lg:text-4xl text-2xl font-bold text-shadow`}>EverVault</h1>
+      <div className="w-1/4 lg:w-2/4  p-3 text-white lg:flex items-center justify-center">
+        <h1 className={`lg:text-6xl text-2xl font-bold text-shadow`}>EverVault</h1>
       </div>
-      <div className="hidden  w-1/4  p-3 text-right lg:flex flex-row-reverse">
-        <div className="hidden lg:w-2/3 lg:flex   justify-end">
-          <IoPerson className="mx-6 cursor-pointer" size={22} />{" "}
-          <FaHeart size={22} className="mx-6 cursor-pointer" />{" "}
-          <FaShoppingBasket size={22} className="mx-6 cursor-pointer" />
+      <div className="hidden  lg:w-1/4  p-3 text-right lg:flex flex-row-reverse">
+        <div className="hidden w-full lg:flex   justify-end">
+          <IoPerson className="mx-6 cursor-pointer" size={30} />{" "}
+          <FaHeart size={30} className="mx-6 cursor-pointer" />{" "}
+          <FaShoppingBasket size={30} className="mx-6 cursor-pointer" />
         </div>
+      </div>
+      <div className="flex lg:hidden w-3/4 justify-end pt-3 mr-16 mt-1">
+      <FaHeart size={30} className="mx-6 cursor-pointer" />{" "}
       </div>
       {/* small devices up intil desktops underneath nav*/}
       <div className="absolute right-0 top-0 flex flex-col lg:hidden text-white">
