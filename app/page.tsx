f96fc3559d43f211bf06b46bd5d2729149ed83d0
Danny Flynn <flynny386@gmail.com>
2024-11-27 10:52:13 +0000

    react icons imported and displayed

--- app/page.tsx
+++ app/page.tsx
@@ -56,8 +56,8 @@ export default function Home() {
       <div className="w-full h-full flex flex-col items-center">
         <NavigationContainer />
         <div className="w-full h-[85%] flex flex-col">
-          <div className="w-full  h-[10%] p-3 flex justify-center lg:justify-end">
-            <div className="w-4/5 sm:w-2/4 lg:w-2/6 2xl:w-1/5 relative my-6 text-black">
+          <div className="w-full   p-3 flex justify-center lg:justify-end">
+            <div className="w-4/5 sm:w-2/4 lg:w-2/6 2xl:w-1/5 relative mt-10 text-black">
               <FaSearch className="absolute left-1 top-2 text-black" size={22} />
               <Input type="search" placeholder="Search..." className="pl-10 py-3 border-2 border-indigo-200 focus:border-indigo-500" />
             </div>
