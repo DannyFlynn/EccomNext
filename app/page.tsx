fa32e06b8c1b6070628a4ac96c61a5149c5b2100
Danny Flynn <flynny386@gmail.com>
2024-11-27 14:28:57 +0000

    deploy failed due to moving component and not cleaning the orinal page with two vars not in use just cleaned

--- app/page.tsx
+++ app/page.tsx
@@ -16,19 +16,12 @@ type Item = {
 }[];
 
 export default function Home() {
-  const [items, setItems] = useState<Item>([]);
-  const cardColors = ["bg-red-200", "bg-gray-300", "bg-indigo-200"];
+
+  
 
   //https://dummyjson.com/products/category/beauty
   //https://dummyjson.com/products?limit=3
-  useEffect(() => {
-    fetch("https://dummyjson.com/products/category/beauty?limit=3")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setItems(data.products);
-      });
-  }, []);
+ 
 
   return (
     <div className="w-full  overflow-x-hidden relative text-white text-shadow">
@@ -66,9 +59,6 @@ export default function Home() {
             <FirstPageProuducts />
           </ProductsContainer>
         </div>
-        {/* <footer className="w-full h-20  block lg:hidden border-t bg-indigo-200 ">
-          I am mob FOOT
-        </footer> */}
       </div>
       <footer className="w-full   border-t bg-indigo-200 flex-col p-3">
         <div className="w-full flex border-b border-white pb-6">
@@ -80,10 +70,9 @@ export default function Home() {
               <div className="flex flex-col mr-6 lg:text-xl">
                 <p>FOLLOW US</p>
                 <div className="flex mt-1">
-                  {/*FaFacebook, FaInstagram, FaGithub*/}
-                  <FaFacebook size={26}/>
-                  <FaInstagram className="ml-3" size={26} />
-                  <FaGithub className="ml-3" size={26}/>
+                  <FaFacebook size={26} className="cursor"/>
+                  <FaInstagram className="ml-3 cursor" size={26}  />
+                  <FaGithub className="ml-3 cursor" size={26} />
                 </div>
               </div>
               <div className="flex flex-col lg:text-xl">
